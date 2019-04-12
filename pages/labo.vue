f6b03cb9ae771bd5160aac70bdb891f47ee10fde
Aysser jbili <aysser21@gmail.com>
2019-04-12 15:38:36 +0100

    Update labo.vue

--- pages/labo.vue
+++ pages/labo.vue
@@ -1,6 +1,5 @@
 
 <template >
-
 <div dir="rtl" id="" style="margin:33px">
 
   <div class="ui special cards">
@@ -13,22 +12,22 @@
             </div>
           </div>
         </div>
-        <div >
-           <nuxt-link to="/simv" style="text-decoration: none;"><img src="~/assets/plante.png"
-              alt="plante" style="border: none;" width="300" height="200" />
+        <div>
+          <nuxt-link to="/simv" style="text-decoration: none;"><img src="~/assets/plante.png" alt="plante" style="border: none;" width="300" height="200" />
             <div style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px; background-color: #FFF; opacity: 0.6; filter: alpha(opacity = 60);"></div>
             <table style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px;">
               <tr>
                 <td style="text-align: center; color: #000; font-size: 24px; font-family: Arial,sans-serif;">اضغط هنا</td>
               </tr>
             </table>
-          </nuxt-link></div>
+          </nuxt-link>
+        </div>
       </div>
       <div class="content">
         <a class="header">النبتة</a>
         <div class="meta">
           <span class="date">
-        أخر تحديث 10 أفريل 2019
+            أخر تحديث 10 أفريل 2019
           </span>
         </div>
       </div>
@@ -37,10 +36,10 @@
           <i class="flask icon"></i>
           1 تجارب
         </span>
-          By
+        By
         <nuxt-link class="by-link" to="/about">
-      Tunisina
-      </nuxt-link>
+          Tunisina
+        </nuxt-link>
       </div>
     </div>
     <div class="card">
@@ -52,23 +51,73 @@
             </div>
           </div>
         </div>
-        <div  >
-           <nuxt-link to="/forse" style="text-decoration: none;"><img src="~/assets/6fors.png"
-              alt="الطاقة" style="border: none;" width="300" height="200" />
+        <div>
+          <nuxt-link to="/forse" style="text-decoration: none;"><img src="~/assets/6fors.png" alt="الطاقة" style="border: none;" width="300" height="200" />
             <div style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px; background-color: #FFF; opacity: 0.6; filter: alpha(opacity = 60);"></div>
             <table style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px;">
               <tr>
                 <td style="text-align: center; color: #000; font-size: 24px; font-family: Arial,sans-serif;">اضغط هنا</td>
               </tr>
             </table>
-          </nuxt-link></div>
+          </nuxt-link>
+        </div>
       </div>
       <div class="content">
         <a class="header">تحول الطاقة </a>
         <div class="meta">
           <span class="date">
 
-        أخر تحديث 10 أفريل 2019
+            أخر تحديث 10 أفريل 2019
+          </span>
+        </div>
+        <span style="color:#6239dc">
+          <i class="ui icon edit small"></i>
+          نسخة معدلة من <strong>Tunisina</strong>
+          النسخة الاصلية <a href="https://phet.colorado.edu/sims/html/energy-forms-and-changes/latest/energy-forms-and-changes_en.html" target="_blank"><strong>
+              هنا
+            </strong>
+          </a>
+
+        </span>
+
+      </div>
+      <div class="extra content">
+        <span>
+          <i class="flask icon"></i>
+          1 تجارب
+        </span>
+        By
+        <nuxt-link class="by-link" to="/phet">
+          PhET Simulations
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <div class="ui dimmer transition">
+          <div class="content">
+            <div class="center">
+
+            </div>
+          </div>
+        </div>
+        <div>
+          <nuxt-link to="/circuit" style="text-decoration: none;"><img src="~/assets/circuit.png" alt="الدارة الكهربائية" style="border: none;" width="300" height="200" />
+            <div style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px; background-color: #FFF; opacity: 0.6; filter: alpha(opacity = 60);"></div>
+            <table style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px;">
+              <tr>
+                <td style="text-align: center; color: #000; font-size: 24px; font-family: Arial,sans-serif;">اضغط هنا</td>
+              </tr>
+            </table>
+          </nuxt-link>
+        </div>
+      </div>
+      <div class="content">
+        <a class="header">الدارة الكهربائية</a>
+        <div class="meta">
+          <span class="date">
+
+            أخر تحديث 10 أفريل 2019
           </span>
         </div>
       </div>
@@ -79,48 +128,8 @@
         </span>
         By
         <nuxt-link class="by-link" to="/phet">
-         PhET Simulations
-      </nuxt-link>
-      </div>
-    </div>
-    <div class="card">
-      <div class="image">
-        <div class="ui dimmer transition">
-          <div class="content">
-            <div class="center">
-
-            </div>
-          </div>
-        </div>
-        <div  >
-           <nuxt-link to="/circuit" style="text-decoration: none;"><img src="~/assets/circuit.png"
-              alt="الدارة الكهربائية" style="border: none;" width="300" height="200" />
-            <div style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px; background-color: #FFF; opacity: 0.6; filter: alpha(opacity = 60);"></div>
-            <table style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px;">
-              <tr>
-                <td style="text-align: center; color: #000; font-size: 24px; font-family: Arial,sans-serif;">اضغط هنا</td>
-              </tr>
-            </table>
-          </nuxt-link></div>
-      </div>
-      <div class="content">
-        <a class="header">الدارة الكهربائية</a>
-        <div class="meta">
-          <span class="date">
-
-        أخر تحديث 10 أفريل 2019
-          </span>
-        </div>
-      </div>
-      <div class="extra content">
-        <span>
-          <i class="flask icon"></i>
-          1 تجارب
-        </span>
-          By
-        <nuxt-link class="by-link" to="/phet">
-       PhET Simulations
-      </nuxt-link>
+          PhET Simulations
+        </nuxt-link>
       </div>
     </div>
   </div>
@@ -139,9 +148,7 @@ export default {
 </script>
 
 <style>
-.by-link{
-  color:#13ab5e !important;
+.by-link {
+  color: #13ab5e !important;
 }
-
-
 </style>
