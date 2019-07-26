a49ee92297d60dbd0e4a0e8186b289276dde9730
aysser <aysser21@gmail.com>
2019-07-26 15:21:35 +0100

    add vuex v1.1

--- pages/labo.vue
+++ pages/labo.vue
@@ -2,7 +2,7 @@
 <div dir="rtl" id="" style="margin:33px">
 
   <div class="ui special  cards">
- <div v-for="sim in sims " :key="sim.id" class=" card" >
+ <div v-for="sim in sims" :key="sim.id" class=" card" >
 
        <div class="image">
          <div class="ui placeholder">
@@ -16,7 +16,7 @@
         </div>
         <div>
             <no-ssr>
-          <nuxt-link :to="sim.link" style="text-decoration: none;"><img :src="'/'+sim.img" alt="" style="border: none;" width="300" height="200" />
+          <nuxt-link :to="'sims/'+sim.link" style="text-decoration: none;"><img :src="'/'+sim.img" alt="" style="border: none;" width="300" height="200" />
             <div style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px; background-color: #FFF; opacity: 0.6; filter: alpha(opacity = 60);"></div>
             <table style="position: absolute; width: 200px; height: 80px; left: 50px; top: 60px;">
               <tr>
@@ -66,20 +66,12 @@ export default {
   components: {
     AppLogo
   },
-data  () {
-    return {
-
- sims: [
-        { id:0, title:"امتصاص النبتة للماء",by:"SimTunisina",date:"24 أفريل 2019",img:"0.png",link:"/peau",desc:"راجع الكتاب المدرسي سنة خامسة صفحة 148" },
-        { id:1, title:"أهمية الجذور بالنسبة لنبة",by:"SimTunisina",date:"24 أفريل 2019",img:"1.png",link:"/pltmort",desc:"موت النبة بدون جذور..." },
-        { id:2, title:"دور الساق في عملية الامتصاص",by:"SimTunisina",date:"24 أفريل 2019",img:"2.png" ,link:"/pcolor",desc:"تغير لون الازهار نتيجة الماء الملون"},
-        { id:3, title:"تبخر الماء",by:"SimTunisina",date:"24 أفريل 2019",img:"3.png",link:"/pltva",desc:"تبخر الماء عن طريق الاوراق.." },
-        { id:4, title:"الدارة العكهربئية",by:"SimTunisina",date:"24 أفريل 2019",img:"circuit.png",link:"/circuit",desc:"بناء الدارة الكهربئية " },
-        { id:5, title:"أشكال الطاقة",by:"SimTunisina",date:"24 أفريل 2019",img:"6fors.png",link:"/forse",desc:"تحول الطاقة من حالة الى اخرى" },
-
-      ]
+  computed:{
+   sims () {
+    return this.$store.state.sims.all
+  }
 }
-}
+
 }
 </script>
 
