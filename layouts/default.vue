9193bf779105ce2936e2fcf56bbbb6edef78a661
Aysser jbili <aysser21@gmail.com>
2019-08-24 01:27:25 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,10 @@
 <template>
   <div :dir="$t('dir')">
-  <no-ssr>
-  <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WL39KHX');</script>
-<!-- End Google Tag Manager -->
- <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-90987178-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-90987178-1');
-</script>
-</no-ssr>
+ 
 <NavBar >
 
 </NavBar>
-<no-ssr>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WL39KHX"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-</no-ssr>
+
 
   <nuxt/>
   
